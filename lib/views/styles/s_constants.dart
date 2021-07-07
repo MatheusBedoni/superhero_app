581ef6793b3d372fc3636a4433a85479369a433d
matheus bedoni <matheus.sousa@sefaz.rr.gov.br>
2021-07-07 14:03:31 -0400

    mudar cor

--- lib/views/styles/s_constants.dart
+++ lib/views/styles/s_constants.dart
@@ -53,7 +53,7 @@ class DecorationConstants{
          borderRadius: BorderRadius.circular(20),
          boxShadow: [
            BoxShadow(
-             color: Colors.black12,
+             color: Colors.black26,
              blurRadius: 4,
              offset: Offset(2, 4), // Shadow position
            ),
